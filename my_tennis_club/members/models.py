fca592fd9006696b0f7da32d3df70563d8d11ba7
Tatiana Bayona <tatianabayona2017@gmail.com>
2025-11-14 10:28:23 -0500

    Se agregan mas datos a la base de datos

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -6,4 +6,10 @@ class Member(models.Model):
     lastname = models.CharField(max_length=255)
     phone=models.IntegerField(null=True)
     joined_date = models.DateField(null=True)
+    email = models.EmailField(max_length=255, null=True)
+    gender = models.CharField(max_length=100, null=True)
+    registration_time = models.DateTimeField(auto_now_add=True, null=True)
+    
+    
+    
     
