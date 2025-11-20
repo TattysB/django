c72224eb5f3a5a38b7c3b430eb4f5fee4b8d06ff
Tatiana Bayona <tatianabayona2017@gmail.com>
2025-11-20 11:09:15 -0500

    Se agrega panel de admin y conexión con DJango

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
-    phone=models.IntegerField(null=True)
+    phone = models.IntegerField(null=True)
     joined_date = models.DateField(null=True)
     email = models.EmailField(max_length=255, null=True)
     gender = models.CharField(max_length=100, null=True)
     registration_time = models.DateTimeField(auto_now_add=True, null=True)
     
-    
-    
-    
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
